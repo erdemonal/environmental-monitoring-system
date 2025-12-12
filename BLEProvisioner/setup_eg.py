f8cf1b6193605d3e470923c210c54d09c4eb267d
erdem <erdemonal11@outlook.com>
2025-12-12 13:50:04 +0100

    vulnerabilities

--- BLEProvisioner/setup_eg.py
+++ BLEProvisioner/setup_eg.py
@@ -6,9 +6,9 @@ TARGET_NAME = "EG-SETUP"
 CREDS_CHAR_UUID = "12345678-1234-5678-1234-56789abc0002"
 
 CREDS = {
-    "ssid": "Dawood",
-    "password": "Dfayad126",
-    "backend_ip": "172.20.10.9",
+    "ssid": "x",
+    "password": "x",
+    "backend_ip": "x",
     "device_key": "demo-device-key"
 }
 
